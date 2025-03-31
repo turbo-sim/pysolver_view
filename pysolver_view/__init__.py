a3d9639bf2484d9d433060a573ca8ebf72d14720
roagr <roagr@dtu.dk>
2025-03-31 19:49:21 +0200

    Bump version: 0.4.4 → 0.4.5

--- pysolver_view/__init__.py
+++ pysolver_view/__init__.py
@@ -25,6 +25,6 @@ from .nonlinear_system import *
 from .nonlinear_system_problems import *
 
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 URL_GITHUB = "https://github.com/turbo-sim/pysolver_view"
 URL_DOCS = "https://turbo-sim.github.io/pysolver_view/"
