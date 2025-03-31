da5b4de110ad66db9ead329cf8b82ae6d6bd26f5
roagr <roagr@dtu.dk>
2025-03-31 11:20:01 +0200

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "pysolver_view"
 copyright = "2024, Roberto Agromayor"
 author = "Roberto Agromayor"
-release = "v0.2.0"
+release = "v0.2.1"
 
 # Define extensions
 extensions = [
